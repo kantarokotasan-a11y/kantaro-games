1285597621cd7e40e70f4dbb49a20db4a4666f33
kantarokotasan-a11y <kantaro.kotasan@gmail.com>
2026-04-21 00:20:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE = 'kantaro-v1';
-const FILES = ['./'];
+const CACHE = 'kantaro-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
