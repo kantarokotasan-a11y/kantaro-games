5c9dd8fbeb9d4959f55afe441d18f329ef3b8874
kantarokotasan-a11y <kantaro.kotasan@gmail.com>
2026-04-24 10:19:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,41 @@
-const CACHE = 'kantaro-v3';
+const CACHE='inv-app-v17';
+const FILES=['./manifest.json','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install', e => {
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(()=>{})));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  const url=new URL(req.url);
+  const isHTML=req.mode==='navigate'||req.destination==='document'||url.pathname.endsWith('.html')||url.pathname.endsWith('/');
+  
+  if(isHTML){
+    e.respondWith(
+      fetch(req).then(resp=>{
+        if(resp.ok){
+          const clone=resp.clone();
+          caches.open(CACHE).then(c=>c.put(req,clone));
+        }
+        return resp;
+      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+    );
+    return;
+  }
+  
+  e.respondWith(
+    caches.match(req).then(r=>r||fetch(req).then(resp=>{
+      if(resp.ok&&req.method==='GET'){
+        const clone=resp.clone();
+        caches.open(CACHE).then(c=>c.put(req,clone));
+      }
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
 });
